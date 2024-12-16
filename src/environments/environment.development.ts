42686310b1cd121160955237513dafd3c4a2c86a
lenny-zanotelli <lenny.zanotelli24@gmail.com>
2024-12-16 10:33:55 +0100

    feat: create a env file and fix config

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiKey:
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZDJmYzgzMWI4OTJhNTgzNjk0ZTY3YmE2YmJjZWZhZSIsIm5iZiI6MTczNDM0MDI4MS4zNTEsInN1YiI6IjY3NWZlZWI5OTZjZmRkYmYxOWNjZDQxYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nYWd5j1RqGAwJ3GBGcl9kypshz9i-H0uxVaBO925sSk',
+  baseUrl: 'https://api.themoviedb.org/3',
+};
